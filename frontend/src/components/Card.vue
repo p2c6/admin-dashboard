1cd142cb0ee2d8a0d572426dfa33ff9ca31f1083
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-10 13:11:50 +0800

    chore: adjust indention

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -12,17 +12,17 @@ defineProps({
 </script>
 
 <template>
-          <div class="card">
-                    <div class="card-header" v-if="header">
-                        <h3 class="card-title">{{ header }}</h3>
-                    </div>
-                    <div class="card-body">
-                       <slot></slot>
-                    </div>
-                    <!-- /.card-body -->
-                    <div class="card-footer" v-if="footer">
-                        {{  footer }}
-                    </div>
-                    <!-- /.card-footer-->
-                    </div>
+    <div class="card">
+        <div class="card-header" v-if="header">
+            <h3 class="card-title">{{ header }}</h3>
+        </div>
+        <div class="card-body">
+            <slot></slot>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer" v-if="footer">
+            {{  footer }}
+        </div>
+        <!-- /.card-footer-->
+    </div>
 </template>
