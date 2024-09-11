f020dabdaad8ad44bda0b5ef6730232743f1efeb
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-11 23:40:18 +0800

    feat: clean up implementation

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -1,54 +1,56 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
+import { defineStore } from 'pinia';
+import { reactive, ref } from 'vue';
 import useAxios from '@/useAxios';
-import { useRouter } from "vue-router";
+import { useRouter } from 'vue-router';
 
 const { http } = useAxios();
 
 export const useAuthStore = defineStore('auth', () => {
-    const router = useRouter();
-    const user = ref(null);
+  const router = useRouter();
+  const user = ref(null); 
 
-    const login = async (credentials) => {
-        const method = credentials.email_username.includes('@') ? 'email' : 'username';
+  const login = async (credentials) => {
+    const method = credentials.email_username.includes('@') ? 'email' : 'username';
 
-        let updatedCredentials = {
-            email: credentials.email_username,
-            password: credentials.password,
-            method: method
-        };
+    let updatedCredentials = {
+      email: credentials.email_username,
+      password: credentials.password,
+      method: method
+    };
 
-        if (method === 'username') {
-            updatedCredentials = {
-                username: credentials.email_username,
-                password: credentials.password,
-                method: method
-            }
-        }
-                
-        const response = await http.post('/api/login', updatedCredentials);
-
-        if (response.status === 200) {
-            const userData = await getUser();
-            user.value = userData
-
-            router.push({name: 'guest'})
-        }
+    if (method === 'username') {
+      updatedCredentials = {
+        username: credentials.email_username,
+        password: credentials.password,
+        method: method
+      };
     }
 
-    const getUser = async() => {
-        try {
-            const { data } = await http.get('/api/user')
+    try {
+      const response = await http.post('/api/login', updatedCredentials);
 
-            return data;            
-        } catch (error) {
-            console.error('Failed getting user:', error)
-        }
+      if (response.status === 200) {
+        await getUser();
+        router.push({name: 'home'})
+      }
+    } catch (error) {
+      console.error('Login error:', error);
     }
+  };
 
-    return {
-        user,
-        login,
-        getUser
+  const getUser = async() => {
+    try {
+        const { data } = await http.get('/api/user');
+        user.value = data;
+    } catch (error) {
+        console.log('Error getting user', error)
+        user.value = null;
     }
-})
+  }
+
+  return {
+    user,
+    login,
+    getUser
+  };
+});
