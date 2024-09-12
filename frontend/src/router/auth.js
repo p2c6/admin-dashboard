e8838016c1db0894c495d88ce7b4ded8dfd30f7b
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-12 18:40:47 +0800

    feat: add route for create product

--- frontend/src/router/auth.js
+++ frontend/src/router/auth.js
@@ -3,6 +3,7 @@ import AuthLayout from '@/components/layouts/AuthLayout.vue';
 import HomeView from '@/views/HomeView.vue';
 import VideosView from '@/views/VideosView.vue';
 import ProductsView from '@/views/ProductsView.vue';
+import CreateProductView from '@/views/CreateProductView.vue';
 
 export default [
   {
@@ -18,6 +19,11 @@ export default [
         path: 'products',
         name: 'products',
         component: ProductsView,
+      },
+      {
+        path: 'products/create',
+        name: 'products.create',
+        component: CreateProductView,
       },
       {
         path: 'videos',
