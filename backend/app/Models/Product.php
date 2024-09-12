7db99ef831d293705544fc7018f2a101ca4966cc
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-12 17:32:54 +0800

    feat: add fillable column

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'category',
+        'description',
+        'date_and_time',
+        'name',
+    ];
 }
