f6722dc5564d10d2ea5e45c3c085a58aded3c132
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-13 18:54:07 +0800

    feat: add read product function; add store product function; add update product function

--- backend/app/Services/Product/ProductService.php
+++ backend/app/Services/Product/ProductService.php
@@ -6,6 +6,18 @@ use Illuminate\Validation\ValidationException;
 
 class ProductService 
 {
+    public function index()
+    {
+        try {
+            $products = Product::all(['name', 'category', 'description', 'date_and_time']);
+
+            return response()->json($products, 200);
+            
+        } catch(\Throwable $th) {
+            info('Error getting product: ' . $th->getMessage());
+            return response()->json(['errors' => 'Server Error'], 500);
+        }
+    }
     public function store($request)
     {
         try {
@@ -27,8 +39,24 @@ class ProductService
         }
     }
 
-    public function update($request)
+    public function update($request, Product $product)
     {
+        try {
+            $product->update([
+                'name' => $request->name,
+                'category' => $request->category,
+                'description' => $request->name,
+                'date_and_time' => $request->dateAndTime,
+            ]);
 
+            return response()->json(['message' => 'Product successfully updated.'], 200);
+
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->errors()], 422);
+
+        } catch (\Throwable $th) {
+            info('Error updating Product: ' . $th->getMessage());
+            return response()->json(['errors' => 'Server Error'], 500);
+        }
     }
 }
