f9df03979ab5aa3192778cba53be59788a44149a
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-14 00:05:10 +0800

    feat: update relation product -> product image

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -15,4 +16,9 @@ class Product extends Model
         'description',
         'date_and_time',
     ];
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
 }
