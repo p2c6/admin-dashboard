91e1da307e655ace193322772b3cf503d8d902df
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-14 00:01:51 +0800

    feat: add summernote component

---
+++ frontend/src/components/SummernoteEditor.vue
@@ -0,0 +1,37 @@
+<!-- SummernoteEditor.vue -->
+<template>
+    <div>
+      <div ref="editor" class="summernote"></div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { onMounted, ref, defineEmits } from 'vue';
+  import $ from 'jquery';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import 'summernote/dist/summernote-bs5.css';
+  import 'summernote/dist/summernote-bs5';
+  
+  // Define emit
+  const emit = defineEmits(['update:modelValue']);
+  
+  const editor = ref(null);
+  
+  onMounted(() => {
+    $(editor.value).summernote({
+      height: 200,
+      callbacks: {
+        onChange: function(contents) {
+          emit('update:modelValue', contents);
+        }
+      }
+    });
+  });
+  </script>
+  
+  <style>
+  .summernote {
+    width: 100%;
+  }
+  </style>
+  
