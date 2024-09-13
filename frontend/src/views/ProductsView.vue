073fc5da5f2ad4eebf17e1869888dd55265c4ce7
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-13 18:55:13 +0800

    feat: implement read all product

--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,53 +1,61 @@
 <script setup>
+import { ref } from 'vue';
 import Content from "@/components/reusables/Content.vue";
 import Card from "@/components/reusables/Card.vue";
-import { onMounted } from "vue";
+import { onMounted,  } from "vue";
 import {  useAuthStore } from "@/stores/auth";
 import { useRouter } from "vue-router";
+import { useProductStore } from "@/stores/product";
+import DataTable from '@/components/DataTable.vue';
 
 const authStore = useAuthStore();
+const productStore = useProductStore();
+
 const router = useRouter();
 
+const headers = ref([
+  { label: 'Name', key: 'name' },
+  { label: 'Category', key: 'category' },
+  { label: 'Description', key: 'description' },
+  { label: 'Date And Time', key: 'date_and_time' },
+  { label: 'Actions', key: 'actions' } 
+]);
+
+
+const rows = ref([]);
+
 onMounted(async() => {
+    
     if (!authStore.user) {
         router.push({name: 'login'})
+    }
+
+    if (productStore.products) {
+        rows.value = productStore.products;
+    } else {
+        rows.value = await productStore.getAllProduct();
     }
 })
 
 </script>
 
 <template>
-    <Content title="Listing of All Post" current="Products">
-        <Card>
-            <RouterLink :to="{name: 'products.create'}">
+    <Content title="Listing of All Product" current="Products">
+        <RouterLink :to="{name: 'products.create'}">
                 <a class="btn btn-primary mb-2">Create</a>
-            </RouterLink>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Address</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>A</td>
-                        <td>B</td>
-                        <td>C</td>
-                    </tr>
-                    <tr>
-                        <td>A</td>
-                        <td>B</td>
-                        <td>C</td>
-                    </tr>
-                    <tr>
-                        <td>A</td>
-                        <td>B</td>
-                        <td>C</td>
-                    </tr>
-                </tbody>
-            </table>
+        </RouterLink>
+        <div v-if="productStore.message" class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ productStore.message }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <Card>
+            <div v-if="rows.length === 0" class="d-flex justify-content-center align-items-center">
+                <div class="spinner-border spinner-border-sm mr-2 text-primary" role="status"></div>
+              <span class="text-primary">Loading...</span>
+            </div>
+            <DataTable v-else :headers="headers" :rows="rows" />
         </Card>
     </Content>
 </template>
