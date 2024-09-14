bb72c4df499f33976cbdf20fe34420c0526a1402
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-14 16:50:01 +0800

    feat: update column

--- backend/database/migrations/2024_09_13_131144_create_product_images_table.php
+++ backend/database/migrations/2024_09_13_131144_create_product_images_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('product_id');
-            $table->string('original_name', 255);
-            $table->string('file_name', 255);
             $table->string('file_path', 255);
             $table->timestamps();
             $table->foreign('product_id')->references('id')->on('products');
