334b5610cc43ced2f3fb4f408fb02312dd6aa680
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-15 16:52:44 +0800

    feat: allow to use in update product

--- frontend/src/components/SummernoteEditor.vue
+++ frontend/src/components/SummernoteEditor.vue
@@ -1,49 +1,54 @@
 <!-- SummernoteEditor.vue -->
-<template>
-    <div>
-      <div ref="editor" class="summernote"></div>
-    </div>
-  </template>
-  
-  <script setup>
-  import { onMounted, ref, defineEmits } from 'vue';
-  import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Ensure the tooltip and popper are included
-  import $ from 'jquery';
-  import 'summernote/dist/summernote-bs5.css';
-  import 'summernote/dist/summernote-bs5';
-  import 'bootstrap/dist/css/bootstrap.min.css';
+<script setup>
+import { onMounted, ref, watch, defineEmits, defineProps } from 'vue';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import $ from 'jquery';
+import 'summernote/dist/summernote-bs5.css';
+import 'summernote/dist/summernote-bs5';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-  
-  // Define emit
-  const emit = defineEmits(['update:modelValue']);
-  
-  const editor = ref(null);
-  
-  onMounted(() => {
-    $(editor.value).summernote({
-      height: 200,
-      tooltip: false,
-      toolbar: [
-        ['style', ['style', 'bold', 'italic', 'underline', 'clear', 'strikethrough']],
+// Define props and emits
+const props = defineProps(['modelValue']);
+const emit = defineEmits(['update:modelValue']);
 
-        ['font', ['fontname', 'fontsize', 'color']],
+const editor = ref(null);
 
-        ['para', ['ul', 'ol', 'paragraph', 'height']],
-
-        ['misc', ['fullscreen']]
-      ],
-      callbacks: {
-        onChange: function(contents) {
-          emit('update:modelValue', contents);
-        }
+onMounted(() => {
+  $(editor.value).summernote({
+    height: 200,
+    tooltip: false,
+    toolbar: [
+      ['style', ['style', 'bold', 'italic', 'underline', 'clear', 'strikethrough']],
+      ['font', ['fontname', 'fontsize', 'color']],
+      ['para', ['ul', 'ol', 'paragraph', 'height']],
+      ['misc', ['fullscreen']]
+    ],
+    callbacks: {
+      onChange: function(contents) {
+        emit('update:modelValue', contents);
       }
-    });
+    }
   });
-  </script>
-  
-  <style>
-  .summernote {
-    width: 100%;
+
+  // Update the editor content when modelValue changes
+  $(editor.value).summernote('code', props.modelValue);
+});
+
+watch(() => props.modelValue, (newValue) => {
+  if ($(editor.value).summernote('isEmpty') || newValue !== $(editor.value).summernote('code')) {
+    $(editor.value).summernote('code', newValue);
   }
-  </style>
-  
+});
+</script>
+
+<template>
+  <div>
+    <div ref="editor" class="summernote"></div>
+  </div>
+</template>
+
+<style>
+.summernote {
+  width: 100%;
+}
+</style>
