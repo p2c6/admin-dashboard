890269a7a5699b3874301e6b85e17865a89088c7
Archie Jefferson Em <archie.em99@gmail.com>
2024-09-15 16:51:26 +0800

    chore: update label

---
+++ frontend/src/views/UpdateProductView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Content from "@/components/reusables/Content.vue";
+import UpdateProductForm from "@/components/forms/UpdateProductForm.vue";
+import { onMounted } from "vue";
+import {  useAuthStore } from "@/stores/auth";
+import { useRouter } from "vue-router";
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+onMounted(async() => {
+    if (!authStore.user) {
+        router.push({name: 'login'})
+    }
+})
+
+</script>
+
+<template>
+    <Content title="Update Product" current="Products">
+      <UpdateProductForm />
+    </Content>
+</template>
